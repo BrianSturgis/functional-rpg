7f8f1b55c5012668e70ff5c6ccf1f91122d49580
Vanessa Su <vnessa.su@gmail.com>
2021-06-23 19:05:51 -0400

    :construction: WIP battle system function for fighting
    Co-authored-by: Brian Sturgis <sturujisu.com>

--- src/js/battle-system.js
+++ src/js/battle-system.js
@@ -1,5 +1,12 @@
 // battle function
 // somehow get monster and player objects
+
+//maybe create a fight function to give to storeStats as the statChangeFunction
+//that means that the fight function will have the player/monster object passed to it
+//have that roll a die to remove some health?
+//some kind of check on health
+//function that is passed the monster that is then passed to the storeStats?
+
 export const rollDice = (numberOfSides) => {
   const min = Math.ceil(1);
   const max = Math.floor(numberOfSides);
