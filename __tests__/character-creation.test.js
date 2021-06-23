70bb80f4935f5214545a713142b3bdeabb035cc0
Vanessa Su <vnessa.su@gmail.com>
2021-06-23 16:23:04 -0400

    :white_check_mark: Add test for charcter creator returns player object
    Co-authored-by: Brian Sturgis <sturujisu.com>

--- __tests__/character-creation.test.js
+++ __tests__/character-creation.test.js
@@ -1,5 +1,12 @@
 import * as character from "../src/js/character-creation.js";
 
 describe("character-creation functions", () => {
-  expect("should return object");
+  it("should return object with class warrior and name Joe", () => {
+    const inputClass = "warrior";
+    const characterClass = character.characterCreator(inputClass);
+    const inputName = "Joe";
+    const playerCharacter = characterClass(inputName);
+    expect(playerCharacter.class).toEqual(inputClass);
+    expect(playerCharacter.name).toEqual(inputName);
+  });
 });
